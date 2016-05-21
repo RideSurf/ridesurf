0e508c153089c73e215a7ba728063b2a6b2c846b
Jack Bennetto <jackbennetto@Jacks-MacBook-Pro-2.local>
2016-05-21 14:34:42 -0700

    add location category,rename interest->category, and moved stuff around

--- database/create_database.sql
+++ database/create_database.sql
@@ -22,6 +22,16 @@ CREATE TABLE profile_review (
     description text
 );
 
+CREATE TABLE category (
+    id serial,
+    name varchar not null
+);
+
+CREATE TABLE profile_category (
+    profile_id integer not null,
+    category_id integer not null
+);
+
 CREATE TABLE trip (
     id serial,
     name varchar, -- human-readable, non-unique name
@@ -43,13 +53,17 @@ CREATE TABLE trip_location (
     position integer not null -- 0 for start, 1 for first stop, ... maybe -1 for end?
 );
 
-
 CREATE TABLE location (
     private boolean not null,
     name varchar not null,
     address varchar not null,
     lat real,
     lon real
+);
+
+CREATE TABLE location_category (
+    location_id integer,
+    category_id integer
 );
 
 -- reviewing location also serves the purpose as marking it as a favorite
@@ -67,22 +81,12 @@ CREATE TABLE profile_location (
     main boolean not null -- main locations are listed first
 );
 
-CREATE TABLE profile_interest (
-    profile_id integer not null,
-    interest_id integer not null
-);
-
-CREATE TABLE interest (
-    id serial,
-    name varchar not null
-);
-
-INSERT INTO interest (name) VALUES ('hiking');
-INSERT INTO interest (name) VALUES ('climbing');
-INSERT INTO interest (name) VALUES ('mountain biking');
-INSERT INTO interest (name) VALUES ('skiing');
-INSERT INTO interest (name) VALUES ('snowboarding');
-INSERT INTO interest (name) VALUES ('snowshoeing');
-INSERT INTO interest (name) VALUES ('surfing');
-INSERT INTO interest (name) VALUES ('urbaneering');
+INSERT INTO category (name) VALUES ('hiking');
+INSERT INTO category (name) VALUES ('climbing');
+INSERT INTO category (name) VALUES ('mountain biking');
+INSERT INTO category (name) VALUES ('skiing');
+INSERT INTO category (name) VALUES ('snowboarding');
+INSERT INTO category (name) VALUES ('snowshoeing');
+INSERT INTO category (name) VALUES ('surfing');
+INSERT INTO category (name) VALUES ('urbaneering');
 
