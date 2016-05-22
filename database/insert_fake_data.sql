4816f6b9647b780a6ebe1f6fb27b7bd462bf8c0d
Cindy Chiao <tcchiao@gmail.com>
2016-05-21 17:18:14 -0700

    add more fake user data

--- database/insert_fake_data.sql
+++ database/insert_fake_data.sql
@@ -3,9 +3,16 @@
 
 \c ridesurf
 
-INSERT INTO profile (username, firstname, lastname, email, car_type) VALUES ('jen', 'Jen', 'Doe', 'jen@ridesurf.co', '2012 Red Prius');
+INSERT INTO profile (username, firstname, lastname, email, car_type) VALUES ('jen', 'Jen', 'Doe', 'jen@ridesurf.co', '2012 Red Toyota Prius');
 INSERT INTO profile (username, firstname, lastname, email, car_type) VALUES ('steve', 'Steve', 'Q', 'steve@ridesurf.co', null);
-INSERT INTO profile (username, firstname, lastname, email, car_type) VALUES ('kat', 'Kat', 'NiCarthy', 'ket@ridesurf.co', null);
+INSERT INTO profile (username, firstname, lastname, email, car_type) VALUES ('kat', 'Kat', 'NiCarthy', 'kat@ridesurf.co', null);
+INSERT INTO profile (username, firstname, lastname, email, car_type) VALUES ('kevin', 'Kevin', 'Wu', 'kevin@gmail.com', '2013 Black Ford Focus');
+INSERT INTO profile (username, firstname, lastname, email, car_type) VALUES ('rachelle', 'Rachelle', 'Stern', 'rachelle@gmail.com', null);
+INSERT INTO profile (username, firstname, lastname, email, car_type) VALUES ('aaron', 'Aaron', 'Tate', 'aaron@ridesurf.co', null);
+INSERT INTO profile (username, firstname, lastname, email, car_type) VALUES ('josslyn', 'Josslyn', 'Parrish', 'josslyn@ridesurf.co', '2016 Blue Subaru Outback');
+INSERT INTO profile (username, firstname, lastname, email, car_type) VALUES ('abby', 'Abby', 'Buckley', 'abby@ridesurf.co', null);
+INSERT INTO profile (username, firstname, lastname, email, car_type) VALUES ('lesley', 'Lesley', 'Rye', 'lesley@ridesurf.co', null);
+INSERT INTO profile (username, firstname, lastname, email, car_type) VALUES ('brendan', 'Brendan', 'Walker', 'brendan@ridesurf.co', '2009 Grey Honda Civic');
 
 -- the ids assume this is inserted in an empty database
 INSERT INTO profile_category (profile_id, category_id) VALUES (1, 3);
@@ -21,6 +28,11 @@ INSERT INTO location (private, name, address, lat, lon) VALUES (True, 'home', 'W
 INSERT INTO location (private, name, address, lat, lon) VALUES (True, 'home', 'Capitol Hill, Seattle, WA', 47.622715, -122.312894);
 INSERT INTO location (private, name, address, lat, lon) VALUES (True, 'home', 'Everett, WA', 48.006060, -122.194113);
 INSERT INTO location (private, name, address, lat, lon) VALUES (True, 'work', '111 S Jackson St, Seattle, WA', 47.599088, -122.333812);
+INSERT INTO location (private, name, address, lat, lon) VALUES (True, 'home', '1605 125th Ave SE, Bellevue, WA', 47.597090, -122.174046);
+INSERT INTO location (private, name, address, lat, lon) VALUES (True, 'home', '209 Kirkland Ave, Kirkland, WA', 47.675428, -122.204621);
+INSERT INTO location (private, name, address, lat, lon) VALUES (True, 'home', '3920 Stone Way N, Seattle, WA', 47.654834, -122.342156);
+INSERT INTO location (private, name, address, lat, lon) VALUES (True, 'home', '912 Dexter Ave N, Seattle, WA', 47.627612, -122.342087);
+
 
 INSERT INTO location_category (location_id, category_id) VALUES (1, 1);
 INSERT INTO location_category (location_id, category_id) VALUES (1, 3);
@@ -29,4 +41,15 @@ INSERT INTO location_category (location_id, category_id) VALUES (1, 5);
 INSERT INTO location_category (location_id, category_id) VALUES (1, 6);
 INSERT INTO location_category (location_id, category_id) VALUES (2, 1);
 INSERT INTO location_category (location_id, category_id) VALUES (3, 8);
+
+INSERT INTO profile_location (profile_id, location_id, name, main) VALUES (1, 4, 'home', 1); 
+INSERT INTO profile_location (profile_id, location_id, name, main) VALUES (2, 5, 'home', 1); 
+INSERT INTO profile_location (profile_id, location_id, name, main) VALUES (3, 6, 'home', 1); 
+INSERT INTO profile_location (profile_id, location_id, name, main) VALUES (2, 7, 'work', 0); 
+INSERT INTO profile_location (profile_id, location_id, name, main) VALUES (4, 8, 'home', 1); 
+INSERT INTO profile_location (profile_id, location_id, name, main) VALUES (5, 9, 'home', 1); 
+INSERT INTO profile_location (profile_id, location_id, name, main) VALUES (6, 10, 'home', 1); 
+INSERT INTO profile_location (profile_id, location_id, name, main) VALUES (7, 11, 'home', 1); 
+
+
 
