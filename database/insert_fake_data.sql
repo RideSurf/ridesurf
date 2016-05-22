d72e4505f5f8a1e17791d8193cf16dadc3f79b9d
Ben Bodenmiller <bbodenmiller@hotmail.com>
2016-05-21 19:38:11 -0700

    add POI fake data

--- database/insert_fake_data.sql
+++ database/insert_fake_data.sql
@@ -32,7 +32,17 @@ INSERT INTO location (private, name, address, lat, lon) VALUES (True, 'home', '1
 INSERT INTO location (private, name, address, lat, lon) VALUES (True, 'home', '209 Kirkland Ave, Kirkland, WA', 47.675428, -122.204621);
 INSERT INTO location (private, name, address, lat, lon) VALUES (True, 'home', '3920 Stone Way N, Seattle, WA', 47.654834, -122.342156);
 INSERT INTO location (private, name, address, lat, lon) VALUES (True, 'home', '912 Dexter Ave N, Seattle, WA', 47.627612, -122.342087);
-
+INSERT INTO location (private, name, address, lat, lon) VALUES (False, 'Lake Dorothy Trailhead','Lake Dorothy Trailhead',47.608468,-121.38611);
+INSERT INTO location (private, name, address, lat, lon) VALUES (False, 'Tonga Ridge Trailhead','Tonga Ridge Trailhead',47.677198,-121.263683);
+INSERT INTO location (private, name, address, lat, lon) VALUES (False, 'Pioneer Camp Trailhead','Pioneer Camp Trailhead',48.679725,-121.897202);
+INSERT INTO location (private, name, address, lat, lon) VALUES (False, 'Turtleback Mountain South Trailhead','Turtleback Mountain South Trailhead',48.641083,-122.977286);
+INSERT INTO location (private, name, address, lat, lon) VALUES (False, 'Orcas Island Eclipse Charters','Orcas Island Eclipse Charters',48.60493,-122.943223);
+INSERT INTO location (private, name, address, lat, lon) VALUES (False, 'Steven's Pass','Steven's Pass',47.744331,-121.090034);
+INSERT INTO location (private, name, address, lat, lon) VALUES (False, 'Peshastin Pinnacles State Park','Peshastin Pinnacles State Park',47.550326,-120.589054);
+INSERT INTO location (private, name, address, lat, lon) VALUES (False, 'Steamboat Rock State Park','Steamboat Rock State Park',47.861185,-119.027663);
+INSERT INTO location (private, name, address, lat, lon) VALUES (False, 'Duthie Hill Park','Duthie Hill Park',47.574604,-121.9798);
+INSERT INTO location (private, name, address, lat, lon) VALUES (False, 'Freund Canyon Trailhead','Freund Canyon Trailhead',47.627333,-120.656184);
+INSERT INTO location (private, name, address, lat, lon) VALUES (False, 'Portland','Portland',45.518781,-122.644819);
 
 INSERT INTO location_category (location_id, category_id) VALUES (1, 1);
 INSERT INTO location_category (location_id, category_id) VALUES (1, 3);
@@ -41,6 +51,21 @@ INSERT INTO location_category (location_id, category_id) VALUES (1, 5);
 INSERT INTO location_category (location_id, category_id) VALUES (1, 6);
 INSERT INTO location_category (location_id, category_id) VALUES (2, 1);
 INSERT INTO location_category (location_id, category_id) VALUES (3, 8);
+INSERT INTO location_category (location_id, category_id) VALUES (12, 1);
+INSERT INTO location_category (location_id, category_id) VALUES (13, 1);
+INSERT INTO location_category (location_id, category_id) VALUES (14, 1);
+INSERT INTO location_category (location_id, category_id) VALUES (15, 1);
+INSERT INTO location_category (location_id, category_id) VALUES (16, 9);
+INSERT INTO location_category (location_id, category_id) VALUES (17, 1);
+INSERT INTO location_category (location_id, category_id) VALUES (17, 3);
+INSERT INTO location_category (location_id, category_id) VALUES (17, 4);
+INSERT INTO location_category (location_id, category_id) VALUES (17, 5);
+INSERT INTO location_category (location_id, category_id) VALUES (17, 6);
+INSERT INTO location_category (location_id, category_id) VALUES (18, 2);
+INSERT INTO location_category (location_id, category_id) VALUES (19, 2);
+INSERT INTO location_category (location_id, category_id) VALUES (20, 3);
+INSERT INTO location_category (location_id, category_id) VALUES (21, 3);
+INSERT INTO location_category (location_id, category_id) VALUES (22, 8);
 
 INSERT INTO profile_location (profile_id, location_id, name, main) VALUES (1, 4, 'home', True); 
 INSERT INTO profile_location (profile_id, location_id, name, main) VALUES (2, 5, 'home', True); 
@@ -50,6 +75,3 @@ INSERT INTO profile_location (profile_id, location_id, name, main) VALUES (4, 8,
 INSERT INTO profile_location (profile_id, location_id, name, main) VALUES (5, 9, 'home', True); 
 INSERT INTO profile_location (profile_id, location_id, name, main) VALUES (6, 10, 'home', True); 
 INSERT INTO profile_location (profile_id, location_id, name, main) VALUES (7, 11, 'home', True); 
-
-
-
